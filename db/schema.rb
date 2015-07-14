ce5dc2c674a5ac13c227290a129a03a6356d3c6c
David Garber & Jocelyn Alsdorf <student@epicodus.com>
2015-07-14 09:27:27 -0700

    Change the schema by deleting a migration and reraking.

--- db/schema.rb
+++ db/schema.rb
@@ -25,10 +25,11 @@ ActiveRecord::Schema.define(version: 20150713184103) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "title"
-    t.string "answer_a"
-    t.string "answer_b"
-    t.date   "timestamps"
+    t.string   "title"
+    t.string   "answer_a"
+    t.string   "answer_b"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
