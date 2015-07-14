3d4b643ca093149d0f73fad41da5b3e6aec27e4b
David Garber & Jocelyn Alsdorf <student@epicodus.com>
2015-07-14 09:46:33 -0700

    Edit the question.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -34,14 +34,15 @@ class QuestionsController < ApplicationController
   def update
     @question = Question.find(params[:id])
     if @question.update_attributes(question_params)
-
+      respond_to do |format|
+        format.html {  redirect_to questions_path }
+        format.js
+      end
     else
       redirect_to  edit_question_path(@question)
     end
-    respond_to do |format|
-      format.html {  redirect_to questions_path }
-      format.js
-    end
+
+
   end
 
   def destroy
